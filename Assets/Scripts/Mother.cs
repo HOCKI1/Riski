680db184f029d8905451857e2fb3058b0e6069fc
Riskiofi <77162793+Riskohop@users.noreply.github.com>
2023-03-11 05:01:53 +0700

    почти переписал

--- Assets/Scripts/Mother.cs
+++ Assets/Scripts/Mother.cs
@@ -5,89 +5,150 @@ using UnityEngine;
 public class Mother : MonoBehaviour
 {
     [SerializeField] Player player;
-    [SerializeField] string room = "MotherRoom";
+    [SerializeField] Animator motherAnimator;
+    [SerializeField] string room;
     [SerializeField] int stageroom = 0;
     [SerializeField] float speedMother;
     [SerializeField] public int hour;
     [SerializeField] int secondsInOneHour;
-    int maxRandom = 80;
+
+
+   // private float randNum = new Random.Range(1,3);
+    //private float Random.Range(1,3)2 = Random.Range(0, 15);
     float timer;
+
     void Start()
     {
+        RoomMother();
         
     }
-
+    bool flag;
     void Update()
     {
+        motherAnimator.SetInteger("stagerRoom", stageroom);
+        switch(room)
+        {
+            case "Hallway":
+                motherAnimator.SetBool("Hallway", true);
+                motherAnimator.SetBool("Tualet", false);
+                motherAnimator.SetBool("Kitchen", false);
+                motherAnimator.SetBool("PlayerRoom", false);
+                motherAnimator.SetBool("MotherRoom", false);
+                break;
+            case "Kitchen":
+                motherAnimator.SetBool("Hallway", false);
+                motherAnimator.SetBool("Tualet", false);
+                motherAnimator.SetBool("Kitchen", true);
+                motherAnimator.SetBool("PlayerRoom", false);
+                motherAnimator.SetBool("MotherRoom", false);
+                break;
+            case "Bathroom":
+                motherAnimator.SetBool("Hallway", false);
+                motherAnimator.SetBool("Tualet", true);
+                motherAnimator.SetBool("Kitchen", false);
+                motherAnimator.SetBool("PlayerRoom", false);
+                motherAnimator.SetBool("MotherRoom", false);
+                break;
+            case "RoomPlayer":
+                motherAnimator.SetBool("Hallway", false);
+                motherAnimator.SetBool("Tualet", false);
+                motherAnimator.SetBool("Kitchen", false);
+                motherAnimator.SetBool("PlayerRoom", true);
+                motherAnimator.SetBool("MotherRoom", false);
+                break;
+            case "RoomMother":
+                motherAnimator.SetBool("Hallway", false);
+                motherAnimator.SetBool("Tualet", false);
+                motherAnimator.SetBool("Kitchen", false);
+                motherAnimator.SetBool("PlayerRoom", false);
+                motherAnimator.SetBool("MotherRoom", true);
+                break;
+
+        }
+       
         timer += Time.deltaTime;
-        if(timer >= secondsInOneHour)
+       
+        if (timer >= secondsInOneHour)
         {
             hour++;
             timer = 0;
         }
     }
 
+    IEnumerator motherMoving()
+    {
+        int randmove = Random.Range(0,3);
+        switch(randmove)
+        {
+            case 0:
+                Invoke("RoomMother", Random.Range(1,3));                
+                break;
+            case 1:
+              
+                Invoke("RoomPlayer", Random.Range(1,3));
+                break;
+            case 2:
+                Invoke("Bathroom", Random.Range(1,3));
+                break;
+            case 3:
+                Invoke("Kitchen", Random.Range(1,3));
+                break;
+        }
+        yield return new WaitForSeconds(Random.Range(1,3));
+    }
+
     public void RoomMother()
     {
-        int i;
-        while(stageroom == 0)
+        RM1();
+        
+        
+    }
+    void RM1()
+    {
+        stageroom = 0;
+        room = "RoomMother";
+        Invoke("RM2", Random.Range(1,3));
+
+    }
+    void RM2()
+    {
+        stageroom++;
+        if (/*Random.Range(0, 1) == 0*/ false)
         {
-            new WaitForSeconds(speedMother);
-            i = Random.Range(0, maxRandom);
-            if (i == 15)
-            {
-                stageroom++;
-            }
+            Invoke("RM1", Random.Range(1,3));
         }
-        while (stageroom == 1)
+        else
         {
-            new WaitForSeconds(speedMother);
-            i = Random.Range(0, maxRandom);
-            if (i == 15)
-            {
-                stageroom++;
-            }
-            if (i == 0)
-            {
-                stageroom--;
-            }
+            Invoke("RM3", Random.Range(1,3));
         }
-        while (stageroom == 2)
+    }
+    void RM3()
+    {
+        stageroom++;
+        Invoke("RM4", Random.Range(1,3));
+    }
+    void RM4()
+    {
+        stageroom++;
+        if (/*Random.Range(0, 1) == 0*/ false)
         {
-            new WaitForSeconds(speedMother);
-            i = Random.Range(0, maxRandom);
-            if (i == 15)
-            {
-                stageroom++;
-            }
+            Invoke("RM1", Random.Range(1,3));
         }
-        while (stageroom == 3)
+        else
         {
-            new WaitForSeconds(speedMother);
-            i = Random.Range(0, maxRandom);
-            if (i == 15)
-            {
-                stageroom++;
-            }
-            if (i == 0)
-            {
-                stageroom = 0;
-            }
+            Invoke("RM5", Random.Range(1,3));
         }
-        while (stageroom == 4)
-        {
-            new WaitForSeconds(speedMother);
-            i = Random.Range(0, maxRandom);
-            if (i == 15)
-            {
-                room = "Hallway";
-                stageroom = 0;
-            }
-        }
+
+    }
+    void RM5()
+    {
+        stageroom++;
+        Invoke("Hallway", Random.Range(1,3));
     }
     public void RoomPlayer()
     {
-        if(player.playerInBed)
+        room = "RoomPlayer";
+        if (player.playerInBed)
         {
             if(player.isActiveMonitor)
             {
@@ -96,6 +157,7 @@ public class Mother : MonoBehaviour
             else
             {
                 room = "Hallway";
+                Hallway();
             }
         }
         else
@@ -103,92 +165,58 @@ public class Mother : MonoBehaviour
             //Game Over
         }
     }
+
     public void Bathroom()
     {
-        int i;
-        while (stageroom == 0)
-        {
-            new WaitForSeconds(speedMother);
-            i = Random.Range(0, maxRandom);
-            if (i == 15)
-            {
-                stageroom++;
-            }
-        }
-        while (stageroom == 1)
-        {
-            new WaitForSeconds(speedMother);
-            i = Random.Range(0, maxRandom);
-            if (i == 15)
-            {
-                stageroom++;
-            }
-        }
-        while (stageroom == 2)
-        {
-            new WaitForSeconds(speedMother);
-            i = Random.Range(0, maxRandom);
-            if (i == 15)
-            {
-                stageroom = 0;
-                room = "Hallway";
-            }
-        }
+        BR1();
+       
+    }
+    void BR1()
+    {
+        stageroom = 0;
+        room = "Bathroom";
+        Invoke("BR2", Random.Range(1,3));
+    }
+    void BR2()
+    {
+        stageroom++;
+        Invoke("BR3", Random.Range(1,3));
+    }
+    void BR3()
+    {
+        stageroom++;
+        Invoke("Hallway", Random.Range(1,3));
+
     }
     public void Kitchen()
     {
-        int i;
-        while(stageroom == 0)
-        {
-            new WaitForSeconds(speedMother);
-            i = Random.Range(0, maxRandom);
-            if (i == 15)
-            {
-                stageroom++;
-            }
-        }
-        while (stageroom == 1)
-        {
-            new WaitForSeconds(speedMother);
-            i = Random.Range(0, maxRandom);
-            if (i == 15)
-            {
-                stageroom++;
-            }
-        }
-        while (stageroom == 2)
-        {
-            new WaitForSeconds(speedMother);
-            i = Random.Range(0, maxRandom);
-            if (i == 15)
-            {
-                stageroom = 0;
-                room = "Hallway";
-            }
-        }
+        K1();
+        
     }
+    void K1()
+    {
+        room = "Kitchen";
+        stageroom = 0;
+        Invoke("K2", Random.Range(1,3));
+    }
+    void K2()
+    {
+        stageroom++;
+        Invoke("K3", Random.Range(1,3));
+    }
+    void K3()
+    {
+        stageroom++;
+        Invoke("Hallway", Random.Range(1,3));
+    }
+   
     public void Hallway()
     {
-        int i;
-        while(stageroom == 0)
-        {
-            new WaitForSeconds(speedMother);
-            i = Random.Range(0, maxRandom);
-            if(i == 15)
-            {
-                i = Random.Range(0, 2);
-                switch(i) {
-                    case 0:
-                        room = "kitchen";
-                        break;
-                    case 1:
-                        room = "Bathroom";
-                        break;
-                    case 2:
-                        room = "RoomPlayer";
-                        break;
-                }
-            }
-        }
+        room = "Hallway";
+        stageroom = 0;
+        StopCoroutine(motherMoving());
+        StartCoroutine(motherMoving());
+        
+
     }
 }
